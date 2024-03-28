ffe88588c9f24503d8c0619938078e42426ccd40
YASH52388 <yashbhoy27@gmail.com>
2024-03-29 00:15:35 +0530

    aboutus chang

--- src/Components/Aboutus.jsx
+++ src/Components/Aboutus.jsx
@@ -9,28 +9,28 @@ function Aboutus() {
         About Us
       </Text>
       <Flex direction="column">
-        <Link href="mailto:your.email@example.com" isExternal mb="2">
+      <Link href="mailto:yashbhoy27@gmail.com" isExternal mb="2">
           <Flex align="center">
             <Icon as={FaEnvelope} mr="2" />
-            <Text>Email: your.email@example.com</Text>
+            <Text>Email: yashbhoy27@gmail.com </Text>
           </Flex>
         </Link>
-        <Link href="https://www.linkedin.com/in/yourusername" isExternal mb="2">
+        <Link href="https://www.linkedin.com/in/yash-bhoy-053231233" isExternal mb="2">
           <Flex align="center">
             <Icon as={FaLinkedin} mr="2" />
-            <Text>LinkedIn: yourusername</Text>
+            <Text>LinkedIn: Yash bhoy</Text>
           </Flex>
         </Link>
-        <Link href="https://github.com/yourusername" isExternal mb="2">
+        <Link href="https://github.com/YASH52388" isExternal mb="2">
           <Flex align="center">
             <Icon as={FaGithub} mr="2" />
-            <Text>GitHub: yourusername</Text>
+            <Text>GitHub: YASH52388</Text>
           </Flex>
         </Link>
-        <Link href="https://twitter.com/yourusername" isExternal>
+        <Link href="https://x.com/yash52388?t=3BMvXVkPGQ7lR7qJyPWVnQ&s=09" isExternal>
           <Flex align="center">
             <Icon as={FaTwitter} mr="2" />
-            <Text>Twitter: yourusername</Text>
+            <Text>Twitter: yash52388</Text>
           </Flex>
         </Link>
       </Flex>
